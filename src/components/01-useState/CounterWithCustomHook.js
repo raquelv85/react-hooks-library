35f27957d713a38ffc0ed42b9b3eef2f2f7b0656
RaquelV <raquelv85@gmail.com>
2021-04-30 21:05:59 +0200

    create customHook useCounter

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import "./counter.css";
+
+import { useCounter } from '../../hooks/useCounter';
 
 export const CounterWithCustomHook = () => {
+
+  const { state, increment, decrement } = useCounter();
+
   return(
     <>
       <h1>Counter with hook: {0}</h1>
