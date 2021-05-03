8fdfd863e46f68ee6566dbf90f0f576065d420a8
RaquelV <raquelv85@gmail.com>
2021-05-03 12:14:06 +0200

    add useFetch to component MultipleCustomHooks

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useFetch } from '../../hooks/useFetch'
+
 import '../02-useEffect/effects.css'
 
 
 export const MultipleCustomHooks = () => {
+
+  const state = useFetch( `https://www.breakingbadapi.com/api/quotes/1` );
+
+  console.log(state)
+
   return (
     <div>
       <h1>Custom hooks!!</h1>
