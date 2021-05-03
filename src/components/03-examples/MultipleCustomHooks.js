16fe1da330a476ea492df521f901d54bc676aa43
RaquelV <raquelv85@gmail.com>
2021-05-03 11:59:55 +0200

    create custom hook and component MultipleCustomHooks

---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../02-useEffect/effects.css'
+
+
+export const MultipleCustomHooks = () => {
+  return (
+    <div>
+      <h1>Custom hooks!!</h1>
+    </div>
+  )
+}
