2ab406c40707cf5c5d57f93793ffd0aa9f6e5073
RaquelV <raquelv85@gmail.com>
2021-05-03 12:07:08 +0200

    add fetch to customHook useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,25 @@
+import React, {useState, useEffect} from 'react';
 
-export const useFetch = () => {
-  
+export const useFetch = (url) => {
+
+  const [ state, setState ] = useState({data: null, loading: true, error: null});
+
+  useEffect(() => {
+
+    fetch( url )
+      .then( resp => resp.json() )
+      .then( data => {
+
+        setState({
+          loading: false,
+          error: null,
+          data
+        })
+
+      })
+
+  }, [url])
+
+  return state;
+
 }
