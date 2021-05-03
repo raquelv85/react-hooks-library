a1b9f64bacd3631746a9bb5b9048e73377b8f21e
RaquelV <raquelv85@gmail.com>
2021-05-03 12:44:15 +0200

    add styles to MultipleCustomHooks

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { useFetch } from '../../hooks/useFetch'
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 
-import '../02-useEffect/effects.css'
-
+import "../02-useEffect/effects.css";
 
 export const MultipleCustomHooks = () => {
+  const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
 
-  const state = useFetch( `https://www.breakingbadapi.com/api/quotes/1` );
-
-  console.log(state)
+  const { author, quote } = !!data && data[0]
 
   return (
     <div>
-      <h1>Custom hooks!!</h1>
+      <h1>Breaking Bad quotes</h1>
+      <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">...Loading</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-0">{ quote }</p>
+          <footer className="blockquote-footer mt-1"> {author} </footer>
+        </blockquote>
+      )}
     </div>
-  )
-}
+  );
+};
