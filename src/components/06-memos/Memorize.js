b5cafe4e52f5c2b48546349d5f8a62888ae851e0
RaquelV <raquelv85@gmail.com>
2021-05-05 13:04:18 +0200

    add useCounter to Memorize component

--- src/components/06-memos/Memorize.js
+++ src/components/06-memos/Memorize.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import '../02-useEffect/effects.css'
+import React from "react";
+import { useCounter } from "../../hooks/useCounter";
+
+import "../02-useEffect/effects.css";
 
 export const Memorize = () => {
+  const { counter, increment } = useCounter(10);
+
   return (
     <div>
       <h1>Memorize</h1>
-      <hr/>
+      <hr />
 
+      <h2>
+        Counter: <small>{counter}</small>
+      </h2>
 
+      <button className="btn btn-primary" onClick={() => increment()}>
+          +1
+      </button>
     </div>
-  )
-}
+  );
+};
