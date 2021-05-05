f0de3c8b349a3bfdce4e201d5c48a4786e47a4cf
RaquelV <raquelv85@gmail.com>
2021-05-05 12:57:49 +0200

    create component Memorize

---
+++ src/components/06-memos/Memorize.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../02-useEffect/effects.css'
+
+export const Memorize = () => {
+  return (
+    <div>
+      <h1>Memorize</h1>
+      <hr/>
+
+
+    </div>
+  )
+}
