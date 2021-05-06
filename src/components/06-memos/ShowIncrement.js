f85d0843bbc8e2427ccbd674f07418b4dd4098ad
RaquelV <raquelv85@gmail.com>
2021-05-06 16:08:04 +0200

    add new components CallbackHook and ShowIncrement

---
+++ src/components/06-memos/ShowIncrement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../02-useEffect/effects.css";
+
+export const ShowIncrement = React.memo(({ increment }) => {
+
+  console.log('render')
+
+  return (
+    <button className="btn btn-primary" onClick={() => increment()}>
+      Incrementar
+    </button>
+  );
+});
