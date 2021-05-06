6f3547876faaa1c6bdf64f21bf8d32101a087abd
RaquelV <raquelv85@gmail.com>
2021-05-06 23:02:09 +0200

    change styles and add html

--- src/components/07-useReducer/TodoApp.js
+++ src/components/07-useReducer/TodoApp.js
@@ -1,27 +1,50 @@
 import React, { useReducer } from "react";
-import { todoReducer } from './todoReducer'
+import { todoReducer } from "./todoReducer";
 
-import './styles.css';
+import "./styles.css";
 
-const initialState = [{
-  id: new Date().getTime(),
-  desc: 'nueva tarea',
-  done: false
-}]
+const initialState = [
+  {
+    id: new Date().getTime(),
+    desc: "nueva tarea",
+    done: false,
+  },
+];
 
 export const TodoApp = () => {
-
-  const [ todos ] = useReducer(todoReducer, initialState)
+  const [todos] = useReducer(todoReducer, initialState);
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
-
-      <ul>
-        <li>Hola</li>
-        <li>Adios</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo) => {
+              return (
+                <li className="list-group-item">
+                  <p>{todo.desc}</p>
+                  <button className="btn btn-danger">Borrar</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar todo</h4>
+          <hr />
+          <form action="">
+            <input
+              className="form-control"
+              type="text"
+              name="description"
+              placeholder="agregar tarea..."
+            />
+            <button className="btn btn-outline-primary mt-1 btn-block">Agregar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
