1caccf2d08575703c3eb895a0f09e388e2035823
RaquelV <raquelv85@gmail.com>
2021-05-06 22:49:32 +0200

    create function todoReducer and add to TodoApp

--- src/components/07-useReducer/TodoApp.js
+++ src/components/07-useReducer/TodoApp.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { todoReducer } from './todoReducer'
 
 import './styles.css';
 
+const initialState = [{
+  id: new Date().getTime(),
+  desc: 'nueva tarea',
+  done: false
+}]
+
 export const TodoApp = () => {
+
+  const [ todos ] = useReducer(todoReducer, initialState)
+
   return (
     <div>
       <h1>TodoApp</h1>
       <hr />
+
+      <ul>
+        <li>Hola</li>
+        <li>Adios</li>
+      </ul>
     </div>
   );
 };
