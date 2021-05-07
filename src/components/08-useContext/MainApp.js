36976cb31c87e6659c07aa4065e6a0b08b3280a9
RaquelV <raquelv85@gmail.com>
2021-05-07 12:11:01 +0200

    create UserContext and add to MainApp

--- src/components/08-useContext/MainApp.js
+++ src/components/08-useContext/MainApp.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-  return <AppRouter />;
+  return (
+    <UserContext.Provider value={}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
--- src/components/08-useContext/NavBar.js
+++ src/components/08-useContext/NavBar.js
@@ -6,9 +6,30 @@ export const NavBar = () => {
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <NavLink exact activeClassName="active" className="nav-item nav-link" to="/">Home</NavLink>
-          <NavLink exact activeClassName="active" className="nav-item nav-link" to="/about">About</NavLink>
-          <NavLink exact activeClassName="active" className="nav-item nav-link" to="/login">Login</NavLink>
+          <NavLink
+            exact
+            activeClassName="active"
+            className="nav-item nav-link"
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            activeClassName="active"
+            className="nav-item nav-link"
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            exact
+            activeClassName="active"
+            className="nav-item nav-link"
+            to="/login"
+          >
+            Login
+          </NavLink>
         </div>
       </div>
     </nav>
