eff1c30ec18777752a820e790c07228ec050a487
RaquelV <raquelv85@gmail.com>
2021-05-07 10:51:38 +0200

    create new component TodoAdd

--- src/components/07-useReducer/TodoApp.js
+++ src/components/07-useReducer/TodoApp.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useEffect } from "react";
 import { todoReducer } from "./todoReducer";
-import { useForm } from "../../hooks/useForm";
 import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
 
 import "./styles.css";
 
@@ -12,34 +12,17 @@ const init = () => {
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: "",
-  });
-
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim().length <= 1) {
-      return;
-    }
-
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
-
+  const handleAddTodo = (newTodo) => {
     const action = {
       type: "add",
       payload: newTodo,
     };
 
     dispatch(action);
-    reset();
   };
 
   const handleDelete = (todoId) => {
@@ -68,24 +51,7 @@ export const TodoApp = () => {
           />
         </div>
         <div className="col-5">
-          <h4>Agregar todo</h4>
-          <hr />
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <input
-              className="form-control"
-              type="text"
-              name="description"
-              placeholder="agregar tarea..."
-              value={description}
-              onChange={handleInputChange}
-            />
-            <button
-              type="submit"
-              className="btn btn-outline-primary mt-1 btn-block"
-            >
-              Agregar
-            </button>
-          </form>
+          <TodoAdd handleAddTodo={(newTodo) => handleAddTodo(newTodo)} />
         </div>
       </div>
     </div>
